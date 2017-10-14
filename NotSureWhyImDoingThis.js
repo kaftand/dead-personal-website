ad6b5d9b6ae169df39bb2d05aca5f8e7ce605d66
kaftand <kaftand@gmail.com>
2017-10-14 18:13:39 -0500

    start from scratch

--- NotSureWhyImDoingThis.js
+++
@@ -1,7 +0,0 @@
-
-d3.selectAll("p").each(function(d, i) {
-  d3.select(this).style("color", "blue")
-  console.log(this)
-})
-console.log(d3.selectAll("p"))
-console.log(d3.select("p").style('color','green'))
