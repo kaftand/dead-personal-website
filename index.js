d0e3182c9a64246ea76e94901a11d0830e34aecc
kaftand <kaftand@gmail.com>
2019-01-25 14:48:28 -0600

    port 80

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/dist'));
 //     res.render("index.html")
 // })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
     console.log('Press Ctrl+C to quit.');
