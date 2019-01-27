31cc3d9d54980c8435bc81fa443627b846d3b439
kaftand <kaftand@gmail.com>
2019-01-27 10:23:58 -0600

    added some messages

--- src/assets/MachineLearning.js
+++ src/assets/MachineLearning.js
@@ -2,14 +2,20 @@ import React from 'react';
 export default (
     <ul>
         <li>
+            <a href="https://kaftand.github.io/GAN-Tutorial/DemoForSeminar.html">
+                GAN Tutorial and Visualization (2017) - Python, Keras
+            </a>
+        </li>
+        <li>
             <a href="https://kaftand.github.io/VisualizingKerasActivations/Visualizing+Keras+Activations.html">
                 Visualizing Keras Activations (2016) - Python, Keras
             </a>
         </li>
         <li>
-            <a href="https://kaftand.github.io/opioid-prescription-visualization/">
+            <a href="https://kaftand.github.io/Glaucomania/IDGlaucoma.html">
                 ANN glaucoma diagnosis (2016) - Python, Keras
             </a>
         </li>
+
     </ul>
 )
