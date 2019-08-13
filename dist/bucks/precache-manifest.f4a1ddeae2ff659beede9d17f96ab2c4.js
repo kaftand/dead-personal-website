d127dc4e1802ea84c9488e1878b082426833d3a0
david <you@example.com>
2019-08-12 21:40:23 -0500

    YOLO

---
+++ dist/bucks/precache-manifest.f4a1ddeae2ff659beede9d17f96ab2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ecb4ff99f11a7d47948376bed46b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b77e84aa99556435ca8",
+    "url": "/static/css/main.2cca14b5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7b77e84aa99556435ca8",
+    "url": "/static/js/main.f110927a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
